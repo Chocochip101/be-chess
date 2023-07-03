99c8de886bb796bc6fbc104f22415eca6a49da8a
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-07-03 17:55:27 +0900

    feat: Pawn & Test 생성

---
+++ src/main/java/softeer2nd/Pawn.java
@@ -0,0 +1,13 @@
+package softeer2nd;
+
+public class Pawn {
+    private String color;
+
+    Pawn(String color){
+        this.color = color;
+    }
+
+    public String getColor(){
+        return this.color;
+    }
+}
