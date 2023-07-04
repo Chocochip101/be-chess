0f646996bb1ded6e7cba5dce41dafc590be2380d
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-07-04 17:19:44 +0900

    feat: initialize(), print() 기능 구현

--- src/main/java/softeer2nd/chess/Board.java
+++ src/main/java/softeer2nd/chess/Board.java
@@ -4,6 +4,7 @@ import softeer2nd.chess.pieces.Pawn;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Board {
     private final List<Pawn> pawnList = new ArrayList<>();
@@ -22,4 +23,74 @@ public class Board {
     public Pawn findPawn(int idx) {
         return pawnList.get(idx);
     }
+
+    public void initialize() {
+        pawnList.clear();
+        for (int line = 0; line < 8; ++line) {
+            pawnList.add(new Pawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION, 6, line));
+            pawnList.add(new Pawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION, 1, line));
+        }
+    }
+
+    public void print() {
+        StringBuilder stringBuilder = new StringBuilder();
+        buildPrintString(stringBuilder);
+        System.out.println(stringBuilder);
+    }
+
+    private void buildPrintString(StringBuilder stringBuilder) {
+        for (int col = 0; col < 8; ++col) {
+            buildRowString(stringBuilder, col);
+            stringBuilder.append("\n");
+        }
+    }
+
+    private void buildRowString(StringBuilder stringBuilder, int rowIndex) {
+        for (int col = 0; col < 8; ++col) {
+            boolean isPawnPresent = isPawnAtPosition(rowIndex, col);
+            if (isPawnPresent) {
+                stringBuilder.append(getPawnSymbol(rowIndex, col));
+            } else {
+                stringBuilder.append(".");
+            }
+        }
+    }
+
+    private boolean isPawnAtPosition(int x, int y) {
+        for (Pawn pawn : pawnList) {
+            if (pawn.getX() == x && pawn.getY() == y) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String getPawnSymbol(int x, int y) {
+        for (Pawn pawn : pawnList) {
+            if (pawn.getX() == x && pawn.getY() == y) {
+                if (pawn.getColor().equals(Pawn.WHITE_COLOR)) {
+                    return "p";
+                } else {
+                    return "P";
+                }
+            }
+        }
+        return "";
+    }
+
+
+    public String getWhitePawnsResult() {
+        return pawnList.stream()
+                .filter(pawn -> pawn.getColor().equals(Pawn.WHITE_COLOR))
+                .map(pawn -> String.valueOf(Pawn.WHITE_REPRESENTATION))
+                .collect(Collectors.joining());
+    }
+
+    public String getBlackPawnsResult() {
+        return pawnList.stream()
+                .filter(pawn -> pawn.getColor().equals(Pawn.BLACK_COLOR))
+                .map(pawn -> String.valueOf(Pawn.BLACK_REPRESENTATION))
+                .collect(Collectors.joining());
+    }
+
 }
--- src/main/java/softeer2nd/chess/pieces/Pawn.java
+++ src/main/java/softeer2nd/chess/pieces/Pawn.java
@@ -2,19 +2,39 @@ package softeer2nd.chess.pieces;
 
 public class Pawn {
     public final static String WHITE_COLOR = "white";
-
     public final static String BLACK_COLOR = "black";
-    private String color;
+    public final static char WHITE_REPRESENTATION = 'p';
+    public final static char BLACK_REPRESENTATION = 'P';
+    private final String color;
+    private final char representation;
+    private int xCord;
+    private int yCord;
 
     public Pawn() {
         this.color = WHITE_COLOR;
+        this.representation = WHITE_REPRESENTATION;
     }
 
-    public Pawn(final String color) {
+    public Pawn(final String color, final Character representation, int xCord, int yCord) {
         this.color = color;
+        this.representation = representation;
+        this.xCord = xCord;
+        this.yCord = yCord;
     }
 
     public String getColor() {
         return this.color;
+    }
+
+    public char getRepresentation() {
+        return this.representation;
+    }
+
+    public int getX(){
+        return this.xCord;
+    }
+
+    public int getY(){
+        return this.yCord;
     }
 }
