9c429b667ec416b74507d59b3693ae034be3d140
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-07-04 10:24:51 +0900

    feat: 색을 나타내는 상수 추가

--- src/main/java/softeer2nd/Pawn.java
+++ src/main/java/softeer2nd/Pawn.java
@@ -1,17 +1,23 @@
 package softeer2nd;
 
 public class Pawn {
-    private String color;
+    public final static Integer WHITE_COLOR = 1;
+    public final static Integer BLACK_COLOR = 2;
+    private Integer color;
 
-    Pawn(final String color){
+    Pawn() {
+        this.color = WHITE_COLOR;
+    }
+
+    Pawn(final Integer color) {
         this.color = color;
     }
 
-    public String getColor(){
+    public Integer getColor() {
         return this.color;
     }
 
-    public boolean verifyPawn(final String color){
+    public boolean verifyPawn(final Integer color) {
         return this.color.equals(color);
     }
 }
--- src/test/java/softeer2nd/PawnTest.java
+++ src/test/java/softeer2nd/PawnTest.java
@@ -1,20 +1,23 @@
 package softeer2nd;
 
 import org.junit.jupiter.api.*;
+
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PawnTest {
+    @Test
+    @DisplayName("색에 맞는 폰이 생성되어야 한다")
+    public void create() {
+        Pawn pawn = new Pawn(Pawn.WHITE_COLOR);
+        assertThat(pawn.verifyPawn(Pawn.WHITE_COLOR)).isEqualTo(true);
+        Pawn secondPawn = new Pawn(Pawn.BLACK_COLOR);
+        assertThat(secondPawn.verifyPawn(Pawn.BLACK_COLOR)).isEqualTo(true);
+    }
 
     @Test
-    @DisplayName("흰색 폰이 생성되어야 한다")
-    public void create() {
-        final String whiteColor = "white";
-        final String blackColor = "black";
-
-        Pawn pawn = new Pawn(whiteColor);
-        assertThat(pawn.verifyPawn(whiteColor)).isEqualTo(true);
-
-        Pawn secondPawn = new Pawn(blackColor);
-        assertThat(secondPawn.verifyPawn(blackColor)).isEqualTo(true);
+    public void create_기본생성자() throws Exception {
+        Pawn pawn = new Pawn();
+        assertEquals(Pawn.WHITE_COLOR, pawn.getColor());
     }
 }
