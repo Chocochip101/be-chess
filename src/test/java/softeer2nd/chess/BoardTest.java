737441ef720846efbbc9282d143f570a38e6140a
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-07-04 10:56:12 +0900

    refactor: Before 어노테이션 중복 테스트 코드 제거

--- src/test/java/softeer2nd/chess/BoardTest.java
+++ src/test/java/softeer2nd/chess/BoardTest.java
@@ -1,5 +1,6 @@
 package softeer2nd.chess;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import softeer2nd.chess.pieces.Pawn;
@@ -8,17 +9,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class BoardTest {
+    Board board;
+    Pawn white;
+    Pawn black;
+
+    @BeforeEach
+    void setup() {
+        board = new Board();
+        white = new Pawn(Pawn.WHITE_COLOR);
+        black = new Pawn(Pawn.BLACK_COLOR);
+    }
+
     @Test
     @DisplayName("체스판에 폰이 추가되는지 확인한다.")
     public void create() throws Exception {
-        Board board = new Board();
-
-        Pawn white = new Pawn(Pawn.WHITE_COLOR);
         board.add(white);
         assertEquals(1, board.size());
         assertEquals(white, board.findPawn(0));
 
-        Pawn black = new Pawn(Pawn.BLACK_COLOR);
         board.add(black);
         assertEquals(2, board.size());
         assertEquals(black, board.findPawn(1));
@@ -27,7 +35,6 @@ public class BoardTest {
     @Test
     @DisplayName("체스판에 폰 이외에 추가 시 컴파일 에러를 발생한다.")
     public void addNotPawn() throws Exception {
-        Board board = new Board();
 
 //        assertThrows(board.add(new Integer("7")));
     }
--- src/test/java/softeer2nd/chess/pieces/PawnTest.java
+++ src/test/java/softeer2nd/chess/pieces/PawnTest.java
@@ -1,5 +1,6 @@
 package softeer2nd.chess.pieces;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import softeer2nd.chess.pieces.Pawn;
@@ -8,11 +9,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PawnTest {
+    Pawn white;
+
+    @BeforeEach
+    void setup() {
+        white = new Pawn();
+    }
+
     @Test
     @DisplayName("색에 맞는 폰이 생성되어야 한다")
     public void create() {
-        Pawn pawn = new Pawn(Pawn.WHITE_COLOR);
-        assertThat(pawn.verifyPawn(Pawn.WHITE_COLOR)).isEqualTo(true);
+        assertThat(white.verifyPawn(Pawn.WHITE_COLOR)).isEqualTo(true);
         Pawn secondPawn = new Pawn(Pawn.BLACK_COLOR);
         assertThat(secondPawn.verifyPawn(Pawn.BLACK_COLOR)).isEqualTo(true);
     }
