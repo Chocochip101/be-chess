3f7d7dc362702fb131e1a490f5cc4f2814364bf8
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-07-05 11:46:36 +0900

    refactor: initialize() before로 이동

--- src/test/java/softeer2nd/chess/BoardTest.java
+++ src/test/java/softeer2nd/chess/BoardTest.java
@@ -3,8 +3,6 @@ package softeer2nd.chess;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import softeer2nd.chess.pieces.Piece;
-import softeer2nd.chess.pieces.Point;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static softeer2nd.chess.utils.StringUtils.appendNewLine;
@@ -12,20 +10,16 @@ import static softeer2nd.chess.utils.StringUtils.appendNewLine;
 
 public class BoardTest {
     private Board board;
-    private Piece white;
-    private Piece black;
 
     @BeforeEach
     void setup() {
         board = new Board();
-        white = Piece.createWhitePawn(new Point(0, 0));
-        black = Piece.createBlackPawn(new Point(1, 1));
+        board.initialize();
     }
 
     @Test
     @DisplayName("getPawnsResult의 결과값을 검증한다.")
     public void initialize() throws Exception {
-        board.initialize();
         assertEquals("pppppppprrnnbbqk", board.getWhitePiecesResult());
         assertEquals("PPPPPPPPRRNNBBQK", board.getBlackPiecesResult());
     }
@@ -33,14 +27,11 @@ public class BoardTest {
     @Test
     @DisplayName("initialize() 실행 시 폰이 16개 생성된다.")
     public void initializePieceCount() {
-        board.add(white);
-        board.initialize();
         assertEquals(board.pieceCount(), 32);
     }
 
     @Test
     public void create() throws Exception {
-        board.initialize();
         String blankRank = appendNewLine("........");
         assertEquals(
                 appendNewLine("RNBQKBNR") +
